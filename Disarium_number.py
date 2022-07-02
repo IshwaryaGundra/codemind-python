abadd7850ef65c12a39fb10faf81b5e6616c978c
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-07-02 23:11:29 +0530

    n=int(input()) k=1 s=0 k=1 l=0 temp=n while n!=0:     d=n%10     n=n//10     s=s*10+d while s!=0:     m=s%10     s=s//10     l=l+m**k    # print(l)     k=k+1 if l==temp:     print("True") else:     print("False")

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,15 +1,20 @@
-n = input()
-len_n = len(n)
-n=int(n)
-copy_n=n
-result = 0
-i = len_n
-while(n!=0):
-    digit = n%10
-    result=result+pow(digit,i)
-    n=int(n/10)
-    i = i - 1
-if(result==copy_n):
+n=int(input())
+k=1
+s=0
+k=1
+l=0
+temp=n
+while n!=0:
+    d=n%10
+    n=n//10
+    s=s*10+d
+while s!=0:
+    m=s%10
+    s=s//10
+    l=l+m**k
+   # print(l)
+    k=k+1
+if l==temp:
     print("True")
 else:
     print("False")
