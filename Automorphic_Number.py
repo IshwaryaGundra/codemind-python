9a5fb71928725ce4520a23045ab6d6661b199ff2
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-07-06 06:38:35 +0530

    n= int(input()) nd = len(str(n)) square = n**2 ld= square%pow(10,nd) if ld==n:   print("Automorphic Number".format(n)) else:   print("Not an Automorphic Number".format(n))

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -5,4 +5,4 @@ ld= square%pow(10,nd)
 if ld==n:  
   print("Automorphic Number".format(n))  
 else:  
-  print("Not an Automorphic Number".format(n))  
+  print("Not an Automorphic Number".format(n))
