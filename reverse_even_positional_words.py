caa1cbcd41605b5e1375229422bb415e325496fb
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-07-07 10:48:20 +0530

    n = input() n = n.split(" ") for i in range(len(n)):     if i%2==0:         for j in range(len(n[i])-1,-1,-1):             print(n[i][j],end="")     else:         for j in range(len(n[i])):             print(n[i][j],end="")     print(end=" ")

--- reverse_even_positional_words.py
+++ reverse_even_positional_words.py
@@ -1,10 +1,10 @@
-s=input()
-x=s.split()
-k=[]
-for i in range(len(x)):
-    if i %2==0:
-        p=x[i]
-        k.append((p[::-1]))
+n = input()
+n = n.split(" ")
+for i in range(len(n)):
+    if i%2==0:
+        for j in range(len(n[i])-1,-1,-1):
+            print(n[i][j],end="")
     else:
-        k.append(x[i])
-print(*k)
+        for j in range(len(n[i])):
+            print(n[i][j],end="")
+    print(end=" ")
