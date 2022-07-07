a7abc13fa56b7ec1e9bfd158ccf2f61dc37f3783
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-07-07 11:00:07 +0530

    N = input() c = input() if c in N:     print(True)     print(N.index(c)) else:     print(False)

--- vowels_in_a_string.py
+++ vowels_in_a_string.py
@@ -1,9 +1,7 @@
-s=input()
-s2=input()
-for i in range(len(s)):
-    if s2==s[i]:
-        print("True")
-        print(i)
-        break
-if s2 not in s:
-    print("False")
+N = input()
+c = input()
+if c in N:
+    print(True)
+    print(N.index(c))
+else:
+    print(False)
