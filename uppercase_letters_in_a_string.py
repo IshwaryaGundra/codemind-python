f1873efb931c9958196e7d6715c0abfeceae6631
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-07-07 11:12:29 +0530

    s=input() l="ABCDEFGHIJKLMNOPQRSTUVWXYZ" c=0 for i in s:     if i in l:         c+=1 print(c)

--- uppercase_letters_in_a_string.py
+++ uppercase_letters_in_a_string.py
@@ -1,6 +1,7 @@
-n=input()
+s=input()
+l="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 c=0
-for i in range(len(n)):
-    if n[i]==n[i].upper() and n[i]!=chr(32):
-        c=c+1
+for i in s:
+    if i in l:
+        c+=1
 print(c)
