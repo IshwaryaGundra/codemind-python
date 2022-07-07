75413b24a16b0fb50576b08a8fbc8fa57ed9cca9
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-07-07 11:22:16 +0530

    s = input() #print(s) c = 0 a = '!@#$^_~./,<>;:"?-\|+=%&*(){}[]`' for i in s:     if i in a:         c+=1 print(c)

--- special_characters_count.py
+++ special_characters_count.py
@@ -1,15 +1,8 @@
-n=input()
-#print(n)
-c=0
-for i in range(len(n)):
-    ch=n[i]
-    if n[i].isalpha():
-        continue
-    elif n[i].isdigit():
-        continue
-    elif n[i]==chr(32):
-        continue
-    else:
-        c=c+1
-        
+s = input()
+#print(s)
+c = 0
+a = '!@#$^_~./,<>;:"?-\|+=%&*(){}[]`'
+for i in s:
+    if i in a:
+        c+=1
 print(c)
