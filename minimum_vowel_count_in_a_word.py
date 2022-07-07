3ec68fddcda2e1c1a8bee372a73b5eef5afc3ebb
IshwaryaGundra <97424580+IshwaryaGundra@users.noreply.github.com>
2022-07-07 11:01:59 +0530

    s = input().lower() s = s.split(' ') m = 99999 for i in range(len(s)):     d = 0     for j in s[i]:         if j in 'aeiou':             d+=1     if d<m:         m=d print(m)

--- minimum_vowel_count_in_a_word.py
+++ minimum_vowel_count_in_a_word.py
@@ -1,11 +1,12 @@
-n=input()
-k=n.split()
-c=0
-m=[]
-for i in k:
-    c=0
-    for j in i:
-        if j in "aeiou":
-            c=c+1
-    m.append(c)
-print(min(m))
+s = input().lower()
+s = s.split(' ')
+m = 99999
+for i in range(len(s)):
+    d = 0
+    for j in s[i]:
+        if j in 'aeiou':
+            d+=1
+    if d<m:
+        m=d
+print(m)
+
